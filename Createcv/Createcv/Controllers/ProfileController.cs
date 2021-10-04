78879fed692eefdd819a4919c959733198b4c02c
mithuncs-18 <mithunchanda1999@gmail.com>
2021-10-04 20:10:10 +0600

    CV

---
+++ Createcv/Createcv/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Createcv.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bio()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
